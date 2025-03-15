9b2e70f75d7b8bdc8ebef451dcf6be38a0b6c9fd
lispking <lispking@qq.com>
2025-03-15 21:58:48 +0800

    fix GoogleAnalytics.tsx cross origin issue

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -9,6 +9,7 @@ export default function GoogleAnalytics() {
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
+        crossOrigin="anonymous"
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
@@ -16,7 +17,12 @@ export default function GoogleAnalytics() {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${GA_MEASUREMENT_ID}');
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+            transport_url: 'https://www.google-analytics.com',
+            cookie_domain: window.location.hostname,
+            cookie_flags: 'SameSite=None;Secure'
+          });
         `}
       </Script>
     </>
