69bb9419df536d333404d106dc51da4dbc1c6065
lispking <lispking@qq.com>
2025-03-15 22:20:51 +0800

    make google analytics component simple

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Script from "next/script";
-import { useEffect } from "react";
 
 const GA_MEASUREMENT_ID = "G-28440BWWYK";
 
@@ -12,51 +11,18 @@ declare global {
 }
 
 export default function GoogleAnalytics() {
-  useEffect(() => {
-    const handleScroll = () => {
-      // 防抖处理滚动事件
-      let timeout: NodeJS.Timeout;
-      return () => {
-        if (timeout) clearTimeout(timeout);
-        timeout = setTimeout(() => {
-          const scrollPercentage = Math.round(
-            (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100
-          );
-          window.gtag("event", "scroll", {
-            transport_url: "https://ssl.google-analytics.com",
-            first_party_collection: true,
-            value: scrollPercentage,
-            page_location: window.location.href,
-          });
-        }, 500);
-      };
-    };
-
-    const debouncedHandleScroll = handleScroll();
-    window.addEventListener("scroll", debouncedHandleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", debouncedHandleScroll);
-    };
-  }, []);
-
   return (
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="lazyOnload"
+        async
       />
-      <Script id="google-analytics" strategy="lazyOnload">
+      <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            page_location: window.location.href,
-            transport_url: 'https://ssl.google-analytics.com',
-            first_party_collection: true,
-            send_page_view: true
-          });
+          gtag('config', '${GA_MEASUREMENT_ID}');
         `}
       </Script>
     </>
